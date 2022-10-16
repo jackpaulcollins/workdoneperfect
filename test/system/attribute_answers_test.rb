7e1b43f47a3db39ea3efa9b4d1b309f58a7e8f6f
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-10-15 20:32:53 -0700

    fix attr answers system tests

--- test/system/attribute_answers_test.rb
+++ test/system/attribute_answers_test.rb
@@ -3,27 +3,28 @@ require "application_system_test_case"
 class AttributeAnswersTest < ApplicationSystemTestCase
   setup do
     @attribute_answer = attribute_answers(:one)
+    @user = users(:one)
+    login_as(@user)
   end
 
   test "visiting the index" do
     visit attribute_answers_url
-    assert_selector "h1", text: "Attribute Answers"
   end
 
-  test "creating a Attribute answer" do
+  test "creating an attribute answer" do
     visit attribute_answers_url
     click_on "New Attribute Answer"
 
     fill_in "Answer", with: @attribute_answer.answer
     fill_in "Employee", with: @attribute_answer.employee_id
-    fill_in "Employee template", with: @attribute_answer.employee_template_id
+    fill_in "Employee template", with: employee_templates(:three).id
     click_on "Create Attribute answer"
 
     assert_text "Attribute answer was successfully created"
     assert_selector "h1", text: "Attribute Answers"
   end
 
-  test "updating a Attribute answer" do
+  test "updating an attribute answer" do
     visit attribute_answer_url(@attribute_answer)
     click_on "Edit", match: :first
 
