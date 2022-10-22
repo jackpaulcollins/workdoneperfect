d1e7b2030651954aa5dd6d98e98ed96708641ce7
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-10-22 16:49:04 -0700

    add postmark creds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = {:api_token => Rails.application.credentials.postmark_api_token}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
