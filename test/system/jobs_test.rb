7421fe12264b42fa49bec47fe5b15abb4da5fee7
Jack Collins <jackcollins@Jacks-MacBook-Air.local>
2022-12-03 17:24:45 -0800

    fix system test

--- test/system/jobs_test.rb
+++ test/system/jobs_test.rb
@@ -4,6 +4,7 @@ class JobsTest < ApplicationSystemTestCase
   setup do
     @job = jobs(:one)
     @user = users(:one)
+    @job_template = job_templates(:one)
     login_as(@user)
   end
 
@@ -17,11 +18,14 @@ class JobsTest < ApplicationSystemTestCase
     click_on "New Job"
     find(".choices__item--selectable").click
     find("#choices--customer-select-item-choice-1").click
+    find("#job_job_template_id").click
+    find("option[value='#{@job_template.id}']").click
     fill_in "Date and time", with: @job.date_and_time
     fill_in "Revenue", with: 100
     fill_in "Estimated hours", with: 8
     fill_in "Total hours", with: 8
     click_on "Create Job"
+
     assert_text "Job was successfully created"
     assert_selector "h1", text: "Jobs"
   end
@@ -29,6 +33,8 @@ class JobsTest < ApplicationSystemTestCase
   test "updating a Job" do
     visit job_url(@job)
     click_on "Edit", match: :first
+    find("#job_job_template_id").click
+    find("option[value='#{@job_template.id}']").click
     fill_in "Estimated hours", with: @job.estimated_hours + 1
     fill_in "Total hours", with: @job.total_hours
     click_on "Update Job"
