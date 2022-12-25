f47a77fcf822a736309eb00ccf5a641a120f545c
Jack Collins <jackpaulcollins@gmail.com>
2022-12-25 09:59:18 -0800

    remove background job for update on jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -47,7 +47,6 @@ class Job < ApplicationRecord
 
   # Broadcast changes in realtime with Hotwire
   after_create_commit -> { broadcast_prepend_later_to :jobs, partial: "jobs/index", locals: {job: self} }
-  after_update_commit -> { broadcast_replace_later_to self }
   after_destroy_commit -> { broadcast_remove_to :jobs, target: dom_id(self, :index) }
 
   scope :completed, ->(completed = true) { where("completed_at IS NOT NULL") if completed }
