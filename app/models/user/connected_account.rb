de5b098b4cb7b4f70bbc38ebcf8ad97ee139ccd1
Jace Bayless <jacebayless@gmail.com>
2023-01-10 21:20:02 -0600

    Figuring out how to do stuff

--- app/models/user/connected_account.rb
+++ app/models/user/connected_account.rb
@@ -25,80 +25,82 @@
 #  fk_rails_...  (user_id => users.id)
 #
 
-class User::ConnectedAccount < ApplicationRecord
-  serialize :auth, JSON
+class User
+  class ConnectedAccount < ApplicationRecord
+    serialize :auth, JSON
 
-  encrypts :access_token
-  encrypts :access_token_secret
+    encrypts :access_token
+    encrypts :access_token_secret
 
-  # Associations
-  belongs_to :user
+    # Associations
+    belongs_to :user
 
-  # Helper scopes for each provider
-  Devise.omniauth_configs.each do |provider, _|
-    scope provider, -> { where(provider:) }
-  end
+    # Helper scopes for each provider
+    Devise.omniauth_configs.each do |provider, _|
+      scope provider, -> { where(provider:) }
+    end
 
-  # Look up from Omniauth auth hash
-  def self.for_auth(auth)
-    where(provider: auth.provider, uid: auth.uid).first
-  end
+    # Look up from Omniauth auth hash
+    def self.for_auth(auth)
+      where(provider: auth.provider, uid: auth.uid).first
+    end
 
-  # Use this method to retrieve the latest access_token.
-  # Token will be automatically renewed as necessary
-  def token
-    renew_token! if expired?
-    access_token
-  end
+    # Use this method to retrieve the latest access_token.
+    # Token will be automatically renewed as necessary
+    def token
+      renew_token! if expired?
+      access_token
+    end
 
-  # Tokens that expire very soon should be consider expired
-  def expired?
-    expires_at? && expires_at <= 30.minutes.from_now
-  end
+    # Tokens that expire very soon should be consider expired
+    def expired?
+      expires_at? && expires_at <= 30.minutes.from_now
+    end
 
-  # Force a renewal of the access token
-  def renew_token!
-    new_token = current_token.refresh!
-    update(
-      access_token: new_token.token,
-      refresh_token: new_token.refresh_token,
-      expires_at: Time.at(new_token.expires_at)
-    )
-  end
+    # Force a renewal of the access token
+    def renew_token!
+      new_token = current_token.refresh!
+      update(
+        access_token: new_token.token,
+        refresh_token: new_token.refresh_token,
+        expires_at: Time.at(new_token.expires_at)
+      )
+    end
 
-  def name
-    auth&.dig("info", "name")
-  end
+    def name
+      auth&.dig("info", "name")
+    end
 
-  def email
-    auth&.dig("info", "email")
-  end
+    def email
+      auth&.dig("info", "email")
+    end
 
-  def image_url
-    auth&.dig("info", "image") || GravatarHelper.gravatar_url_for(email)
-  end
+    def image_url
+      auth&.dig("info", "image") || GravatarHelper.gravatar_url_for(email)
+    end
 
-  private
+    private
 
-  def current_token
-    OAuth2::AccessToken.new(
-      strategy.client,
-      access_token,
-      refresh_token:
-    )
-  end
+    def current_token
+      OAuth2::AccessToken.new(
+        strategy.client,
+        access_token,
+        refresh_token:
+      )
+    end
 
-  def strategy
-    # First check the Jumpstart providers for credentials
-    provider_config = Jumpstart::Omniauth.enabled_providers[provider.to_sym]
+    def strategy
+      # First check the Jumpstart providers for credentials
+      provider_config = Jumpstart::Omniauth.enabled_providers[provider.to_sym]
 
-    # Fallback to the Rails credentials
-    provider_config ||= Rails.application.credentials.dig(:omniauth, provider.to_sym)
+      # Fallback to the Rails credentials
+      provider_config ||= Rails.application.credentials.dig(:omniauth, provider.to_sym)
 
-    OmniAuth::Strategies.const_get(OmniAuth::Utils.camelize(provider).to_s).new(
-      nil,
-      provider_config[:public_key], # client id
-      provider_config[:private_key] # client secret
-    )
+      OmniAuth::Strategies.const_get(OmniAuth::Utils.camelize(provider).to_s).new(
+        nil,
+        provider_config[:public_key], # client id
+        provider_config[:private_key] # client secret
+      )
+    end
   end
 end
--- app/notifications/account/ownership_notification.rb
+++ app/notifications/account/ownership_notification.rb
@@ -1,29 +1,31 @@
 # frozen_string_literal: true
 
-class Account::OwnershipNotification < ApplicationNotification
-  deliver_by :action_cable, format: :to_websocket, channel: "NotificationChannel"
+class Account
+  class OwnershipNotification < ApplicationNotification
+    deliver_by :action_cable, format: :to_websocket, channel: "NotificationChannel"
 
-  # Name of the previous owner. We only use the name in case the user gets deleted.
-  params :previous_owner
+    # Name of the previous owner. We only use the name in case the user gets deleted.
+    params :previous_owner
 
-  # Account being transferred
-  params :account
+    # Account being transferred
+    params :account
 
-  def to_websocket
-    {
-      html: ApplicationController.render(partial: "notifications/notification", locals: {notification: record})
-    }
-  end
+    def to_websocket
+      {
+        html: ApplicationController.render(partial: "notifications/notification", locals: {notification: record})
+      }
+    end
 
-  def message
-    t "notifications.account_transferred", previous_owner: params[:previous_owner], account: record.account.name
-  end
+    def message
+      t "notifications.account_transferred", previous_owner: params[:previous_owner], account: record.account.name
+    end
 
-  def url
-    account_path(record.account)
-  end
+    def url
+      account_path(record.account)
+    end
 
-  def user
-    params[:user]
+    def user
+      params[:user]
+    end
   end
 end
