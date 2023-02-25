8544e76fa53b606f2d73865443167fa1ffe25737
Jack Collins <jackpaulcollins@gmail.com>
2023-02-25 00:26:41 -0800

    switch to fullcalendar, no need to reinvent the wheel

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,7 +3,7 @@ class CalendarsController < ApplicationController
     jobs = Job.all.map do |job|
       {
         id: job.id,
-        resourceId: 'a',
+        resourceId: "a",
         title: job.customer.email,
         start: job.date_and_time.iso8601,
         end: (job.date_and_time + 8.hours).iso8601
