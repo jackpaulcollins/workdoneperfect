8ceead30b58d0c0d9f0e7a420f9d084dd13362d9
Jack Collins <jackpaulcollins@gmail.com>
2023-03-05 17:27:24 -0800

    rubocop fix

--- db/seeds/users.seeds.rb
+++ db/seeds/users.seeds.rb
@@ -1,13 +1,13 @@
-frozen_string_literal: true
+# frozen_string_literal: true
 
 USERS = [
   {
-    name: "Jace Bayless",
-    email: "jacebayless@gmail.com"
+    name: 'Jace Bayless',
+    email: 'jacebayless@gmail.com'
   },
   {
-    name: "Jack Collins",
-    email: "jackpaulcollins@gmail.com"
+    name: 'Jack Collins',
+    email: 'jackpaulcollins@gmail.com'
   }
 ].freeze
 
@@ -15,11 +15,11 @@ USERS.each do |user|
   next if User.find_by(email: user[:email])
 
   User.create({
-    name: user[:name],
-    email: user[:email],
-    password: "password",
-    password_confirmation: "password",
-    admin: true,
-    terms_of_service: true
-  })
+                name: user[:name],
+                email: user[:email],
+                password: 'password',
+                password_confirmation: 'password',
+                admin: true,
+                terms_of_service: true
+              })
 end
