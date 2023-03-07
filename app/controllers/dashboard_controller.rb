0dba1ebe9a1d899331608369ec61fcafd6b7de1a
Jack Collins <jackpaulcollins@gmail.com>
2023-03-06 20:36:29 -0800

    improve the js but it still sucks

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
+  helper_method :date_range
+
   def show
     render controller: "CalendarsController", action: "show"
   end
 
   def daily_data
-    render json: {data: params[:dates]}
+    respond_to do |format|
+      format.html { render partial: "dashboard/daily_data", locals: {date_range: date_range} }
+      format.json { render json: {data: params[:dates]} }
+    end
+  end
+
+  def date_range
+    params[:date] ||= Date.today
   end
 end
--- app/javascript/controllers/daily_data_controller.js
+++ app/javascript/controllers/daily_data_controller.js
@@ -1,11 +1,20 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
   static outlets = ['fullcalendar'];
 
+  static targets = ['dateDisplay'];
+
   connect() {
     this.fullcalendarOutlet.calendar.on('datesSet', () => {
       const dateString = this.fullcalendarOutlet.calendar.currentData.viewTitle;
+      const dateObj = new Date(dateString);
+      const month = dateObj.toLocaleString('default', { month: 'long' });
+      const day = dateObj.getDate();
+      const ordinal = this.getOrdinal(day);
+      const dayStr = `${day}${ordinal}'s`;
+      const formattedDate = `${month} ${dayStr} revenue`;
+      this.dateDisplayTarget.innerHTML = formattedDate;
       this.fetchData(dateString);
     });
   }
@@ -15,5 +24,21 @@ export default class extends Controller {
     const response = await fetch(`/dashboard/daily_data.json?dates=${dateString}`);
     const data = await response.json();
     console.log(data);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getOrdinal(day) {
+    const ones = day % 10;
+    const tens = Math.floor(day / 10) % 10;
+    if (tens === 1) {
+      return 'th';
+    } if (ones === 1) {
+      return 'st';
+    } if (ones === 2) {
+      return 'nd';
+    } if (ones === 3) {
+      return 'rd';
+    }
+    return 'th';
   }
 }
